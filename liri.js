3118b485e140bea755cd4ef035173e5493bfb5d9
Jeff Crawford <jeffrey.a.crawford@gmail.com>
2018-04-06 13:28:21 -0400

    npms?

--- liri.js
+++ liri.js
@@ -1,4 +1,20 @@
 require("dotenv").config();
+var request = require("request");
+var twitter = require("twitter");
+
+
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+input = process.argv[2]
+
+twitter.get(input, function (err, results) {
+    if (err) {
+       console.log(err)
+    }
+
+   
+    console.log(JSON.stringify(results, null, 2))
+
+
+})
